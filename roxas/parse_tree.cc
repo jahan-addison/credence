65b9d91b83b5bb4496dc8abb0da9279bff8655a5
Jahan Addison <jahan@jahanaddison.com>
2024-12-09 16:41:13 -0500

    chore: parse tree loader

--- roxas/parse_tree.cc
+++ roxas/parse_tree.cc
@@ -1,67 +1,7 @@
 
-#include <parse_tree.h>
+#include <roxas/parse_tree.h>
 
-std::string get_parse_tree(std::string const& file_path)
-{
-    PyObject *pName, *pModule, *pFunc;
-    PyObject *pArgs, *pValue;
-    int i;
+// https://docs.python.org/3/extending/embedding.html#pure-embedding
+#include <Python.h>
 
-    if (argc < 3) {
-        fprintf(stderr, "Usage: call pythonfile funcname [args]\n");
-        return 1;
-    }
-
-    Py_Initialize();
-    pName = PyUnicode_DecodeFSDefault(std::string(file_path));
-    /* Error checking of pName left out */
-
-    pModule = PyImport_Import(pName);
-    Py_DECREF(pName);
-
-    if (pModule != NULL) {
-        pFunc = PyObject_GetAttrString(pModule, argv[2]);
-        /* pFunc is a new reference */
-
-        if (pFunc && PyCallable_Check(pFunc)) {
-            pArgs = PyTuple_New(argc - 3);
-            for (i = 0; i < argc - 3; ++i) {
-                pValue = PyLong_FromLong(atoi(argv[i + 3]));
-                if (!pValue) {
-                    Py_DECREF(pArgs);
-                    Py_DECREF(pModule);
-                    fprintf(stderr, "Cannot convert argument\n");
-                    return 1;
-                }
-                /* pValue reference stolen here: */
-                PyTuple_SetItem(pArgs, i, pValue);
-            }
-            pValue = PyObject_CallObject(pFunc, pArgs);
-            Py_DECREF(pArgs);
-            if (pValue != NULL) {
-                printf("Result of call: %ld\n", PyLong_AsLong(pValue));
-                Py_DECREF(pValue);
-            } else {
-                Py_DECREF(pFunc);
-                Py_DECREF(pModule);
-                PyErr_Print();
-                fprintf(stderr, "Call failed\n");
-                return 1;
-            }
-        } else {
-            if (PyErr_Occurred())
-                PyErr_Print();
-            fprintf(stderr, "Cannot find function \"%s\"\n", argv[2]);
-        }
-        Py_XDECREF(pFunc);
-        Py_DECREF(pModule);
-    } else {
-        PyErr_Print();
-        fprintf(stderr, "Failed to load \"%s\"\n", argv[1]);
-        return 1;
-    }
-    if (Py_FinalizeEx() < 0) {
-        return 120;
-    }
-    return 0;
-}
+void ParseTreeLoader::parse() {}
--- roxas/parse_tree.h
+++ roxas/parse_tree.h
@@ -1,8 +1,46 @@
-
 #pragma once
 
-#include <string_view>
-// https://docs.python.org/3/extending/embedding.html#pure-embedding
-#include <Python.h>
+// https://github.com/hackable-devices/polluxnzcity/blob/master/PolluxGateway/include/pollux/pollux_extension.h
 
-std::string get_parse_tree(std::string const& file_path);
+#include <string>
+#include <string_view>
+
+namespace roxas {
+
+namespace detail {
+/**
+ * @brief
+ *
+ * Data structure of parse tree.
+ *
+ */
+struct ParseTree
+{
+    std::string source;
+};
+
+} // namespace detail
+
+/**
+ * @brief
+ *
+ * Parse tree loader via python interface to the python compiler frontend.
+ *
+ */
+class ParseTreeLoader
+{
+  public:
+    ParseTreeLoader(ParseTreeLoader const&) = delete;
+    ParseTreeLoader(std::string_view file_path)
+        : file_path_(file_path);
+
+    void parse();
+
+    ~ParseTreeLoader();
+
+  private:
+    std::string_view file_path_;
+    detail::ParseTree tree_{};
+};
+
+} // namespace roxas
