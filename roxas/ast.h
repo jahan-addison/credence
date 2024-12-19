ca3d7441dbc2344f719a90b00bd2da34f8a3f339
Jahan Addison <info@jacata.me>
2024-12-18 19:44:55 -0500

    chore(ast): ast type improvements

--- roxas/ast.h
+++ roxas/ast.h
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) Jahan Addison
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #pragma once
 
 #include <memory>
@@ -34,22 +49,22 @@ class Abstract_Syntax_Tree
 
 enum class literal : int
 {
-    NUMBER,
-    CONSTANT,
-    STRING,
-    UNKNOWN
+    Number,
+    Constant,
+    String,
+    Unknown
 };
 
 struct definition
 {
     enum class type : int
     {
-        FUNCTION,
-        VECTOR,
-        UNKNOWN
+        Function,
+        Vector,
+        Unknown
     };
 
-    type type{ type::UNKNOWN };
+    type type{ type::Unknown };
     std::vector<node> children{};
 };
 
