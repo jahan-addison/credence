eabbc9280f56f6bf9e908333ca0f590f2748c5fa
Jahan Addison <info@jacata.me>
2025-12-13 18:56:01 -0500

    feat: installation script for linux

--- bin/install.sh
+++ bin/install.sh
@@ -29,7 +29,7 @@ You may need to add the following lines to your shell environment:
 
 export CXX=/usr/bin/c++
 export SDKROOT="\$(xcrun --sdk macosx --show-sdk-path)"
-export PYENV_ROOT="\$HOME/.pyenv"
+export PATH=\$PATH:\$HOME/.local/bin:
 
 You might also need LLVM setup in your \$PATH:
 
@@ -38,47 +38,49 @@ $(printf '\033[0m')
 EOM
 )
 
-if [[ "$UNAMESTR" == 'Linux' ]]; then
+Python3_ROOT="$HOME/.local/share/uv/python/cpython-3.14.2-*/"
 
+if [[ "$UNAMESTR" == 'Linux' ]]; then
   sudo apt-get update
-  wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
-  sudo add-apt-repository "deb http://apt.llvm.org/$(lsb_release -cs)/ llvm-toolchain-$(lsb_release -cs) main"
-  sudo apt-get install -y gcc-10 llvm-20 valgrind clang-20 iwyu python3-dev cppcheck binutils cmake clang-tidy pipx
-  echo 'eval "$(register-python-argcomplete pipx)"' >> ~/.bashrc
-  pipx install poetry
+  wget https://apt.llvm.org/llvm.sh
+  chmod +x llvm.sh
+  sudo ./llvm.sh 20
+  sudo apt-get install clang-20 llvm-20 lldb-20 lld-20 libc++-20-dev libc++abi-20-dev
+  sudo apt-get install -y python3-dev libpython3-dev gcc-10 valgrind iwyu cppcheck binutils cmake clang-tidy
+  python3 -m pip install --user pipx
+  sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang++-20 100
+  sudo update-alternatives --install /usr/bin/cc cc /usr/bin/clang-20 100
   sudo update-alternatives \
     --install /usr/bin/gcc gcc /usr/bin/gcc-10 100 \
     --slave /usr/bin/gcc-ar gcc-ar /usr/bin/gcc-ar-10 \
     --slave /usr/bin/gcc-ranlib gcc-ranlib /usr/bin/gcc-ranlib-10 \
     --slave /usr/bin/gcov gcov /usr/bin/gcov-10
-  sudo update-alternatives \
-    --install /usr/bin/llvm-ar llvm-ar /usr/bin/llvm-ar-20 100 \
-    --slave /usr/bin/llvm-ranlib llvm-ranlib /usr/bin/llvm-ranlib-20 \
-    --slave /usr/bin/llvm-cov llvm-cov /usr/bin/llvm-cov-20
-  sudo update-alternatives \
-    --install /usr/bin/clang clang /usr/bin/clang-20 100
-
-  cmake -Bbuild -DCMAKE_CXX_COMPILER="$(which clang++)" -DIWYU=OFF -DCMAKE_BUILD_TYPE=Debug -DUSE_SANITIZER="Address;Undefined" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+  cmake -Bbuild -DPython3_ROOT_DIR="$Python3_ROOT" -DCMAKE_CXX_FLAGS="-stdlib=libc++" -DCMAKE_CXX_COMPILER="$(which clang++-20)" -DIWYU=OFF -DCMAKE_BUILD_TYPE=Debug -DUSE_SANITIZER="Address;Undefined" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 
 elif [[ "$UNAMESTR" == 'Darwin' ]]; then
   brew install include-what-you-use pipx include-what-you-use llvm@20 cmake gcc clang-format cppcheck coreutils poetry
 
   echo -e "$MACOS_DETAILS"
 
-  cmake -Bbuild -DCMAKE_CXX_COMPILER=/usr/bin/c++ -DIWYU=OFF -DCMAKE_BUILD_TYPE=Debug -DUSE_SANITIZER="Address;Undefined" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+  cmake -Bbuild -DPython3_ROOT_DIR="$Python3_ROOT" -DCMAKE_CXX_FLAGS="-stdlib=libc++" -DCMAKE_CXX_COMPILER=c++ -DCMAKE_CXX_STANDARD=20 -DIWYU=OFF -DCMAKE_BUILD_TYPE=Debug -DUSE_SANITIZER="Address;Undefined" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
 fi
 
-echo -e "\033[38;5;113mInstalling the Lexing and Parsing Python submodule...\033[0m"
+echo -e "\033[38;5;113mInstalling the Lexer and Parser Python submodule...\033[0m"
 
+pipx install uv
+uv venv --python 3.14.2 --clear
+uv tool install pip
+source .venv/bin/activate
 git submodule update --init --recursive
+
 # shellcheck disable=SC2164
 cd python/chakram
-poetry install
-make install
+uv pip install .
 
-cd ../../
+cd $CREDENCE
 
 echo "$(pwd)" > $HOME/.credence
+
 cmake --build build
 chmod +x ./bin/credence
 
